23d453ab866e65a2d5a39bb01565b710376b3eac
Castilla454 <castilla4454@gmail.com>
2024-04-17 10:45:10 +0200

    hola

---
+++ src/exercicis_formatmd/exercici_tres/javadocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"exercicis_formatmd.exercici_tres","l":"Empleat"},{"p":"exercicis_formatmd.exercici_tres","l":"EmpleatTest"}];updateSearchResults();
